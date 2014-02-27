0274d8f9819da7562fb798ac0c1c79d49c6c5178
Jordan Giacone <Jmgiacone@gmail.com>
2014-02-26 19:31:46 -0600

    Renamed Clock

--- ClockAngles/ClockAngles.cpp
+++ ClockAngles/ClockAngles.cpp
@@ -35,13 +35,14 @@ void calcAngle(int hour, int minute, int second)
   hour *= SECONDS_IN_HOUR;
   minute *= SECONDS_IN_MINUTE;
   
-  hour = minute + second;
+  hour = hour + minute + second;
   minute += second;
 
   hourAngle = 360 * (hour / SECONDS_ON_CLOCK);
   minuteAngle = 360 * ((minute * MINUTE_HAND_AROUND_CLOCK) / SECONDS_ON_CLOCK);
   secondAngle = 360 * ((second * SECOND_HAND_AROUND_CLOCK) / SECONDS_ON_CLOCK);
-  
+ 
+  cout << hourAngle << " " << minuteAngle << " " << secondAngle << endl; 
   /*if(hourAngle > 180)
   {
     hourAngle = 360 - hourAngle;
@@ -55,6 +56,15 @@ void calcAngle(int hour, int minute, int second)
     secondAngle = 360 - secondAngle
   }*/
 
+  if(hourAngle > 180 && minuteAngle < 180)
+  {}
+  if(hourAngle < 180 && minuteAngle > 180)
+  {}
+  if(hourAngle < 180 && minuteAngle < 180)
+  {}
+  if(hourAngle > 180 && minuteAngle > 180)
+  {}
+  
   hourToMin = hourAngle + minuteAngle;
   hourToSec = hourAngle + secondAngle;
   minToSec = minuteAngle + secondAngle;
